d5b1a3f050b58dab9ef0c68fac5ecc0ab4bfed27
Vladimir-Kuchinskiy <kuchinskiy.wladimir@gmail.com>
2018-06-26 13:59:48 +0300

    lesson-15 done

--- app/models/route.rb
+++ app/models/route.rb
@@ -4,5 +4,14 @@ class Route < ApplicationRecord
   has_many :railway_stations_routes
   has_many :railway_stations, through: :railway_stations_routes
   has_many :trains
+
   validates :name, presence: true
+
+  before_validation :set_name
+
+  private
+
+  def set_name
+    self.name ||= "#{railway_stations.first.title} - #{railway_stations.last.title}"
+  end
 end

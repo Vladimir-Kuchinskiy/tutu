d229dd252d1ac3c78641d6ff9d5fd7fa4a4c6ccf
Vladimir-Kuchinskiy <kuchinskiy.wladimir@gmail.com>
2018-07-07 23:47:10 +0300

    email locale fixing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,20 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Config actionmailer for heroku
+  config.action_mailer.default_url_options = { host: 'serene-everglades-50908.herokuapp.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :domain => 'mail.gmail.com',
+      :address => 'smtp.sendgrid.net',
+      :port => 587,
+      :authentication => :plain,
+      :enable_starttls_auto => true
+  }
+
 end
